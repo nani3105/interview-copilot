4dea3d90a3b2f121f91b0c47bd597c6c4018f2a7
Z003W1F <Niranjan.Kurra@target.com>
2026-03-19 00:24:32 -0500

    fix: allow underscore-prefixed vars in no-unused-vars ESLint rule

--- apps/desktop/.eslintrc.cjs
+++ apps/desktop/.eslintrc.cjs
@@ -14,7 +14,7 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'warn',
   },
